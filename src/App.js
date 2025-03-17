39e5e050541c39762a3d1689f3568e9acedc82f8
SlavikMelnyk <yarikmelnyk39@gmail.com>
2025-03-17 12:34:11 +0200

    create web rtc app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react"
+import Peer from "simple-peer"
+import io from "socket.io-client"
+import "./App.css"
 
+
+const socket = io.connect('http://localhost:5001')
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ me, setMe ] = useState("")
+	const [ stream, setStream ] = useState()
+	const [ receivingCall, setReceivingCall ] = useState(false)
+	const [ caller, setCaller ] = useState("")
+	const [ callerSignal, setCallerSignal ] = useState()
+	const [ callAccepted, setCallAccepted ] = useState(false)
+	const [ idToCall, setIdToCall ] = useState("")
+	const [ callEnded, setCallEnded] = useState(false)
+	const [ name, setName ] = useState("")
+	const myVideo = useRef()
+	const userVideo = useRef()
+	const connectionRef= useRef()
+
+	useEffect(() => {
+		navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+			.then((stream) => {
+				setStream(stream);
+				if (myVideo.current) {
+					setTimeout(() => {
+						myVideo.current.srcObject = stream;
+					}, 100);
+				} else {
+					console.error("myVideo reference is not set yet.");
+				}
+			})
+			.catch((error) => {
+				console.error("Error accessing media devices.", error);
+			});
+
+	socket.on("me", (id) => {
+			setMe(id)
+		})
+
+		socket.on("callUser", (data) => {
+			setReceivingCall(true)
+			setCaller(data.from)
+			setName(data.name)
+			setCallerSignal(data.signal)
+		})
+	}, [])
+
+	const callUser = (id) => {
+		const peer = new Peer({
+			initiator: true,
+			trickle: false,
+			stream: stream
+		})
+		peer.on("signal", (data) => {
+			socket.emit("callUser", {
+				userToCall: id,
+				signalData: data,
+				from: me,
+				name: name
+			})
+		})
+		peer.on("stream", (stream) => {
+			if (userVideo.current) {
+				setTimeout(() => {
+					userVideo.current.srcObject = stream;
+				}, 100);
+			} else {
+				console.error("userVideo reference is not set yet.");
+			}
+		})
+		socket.on("callAccepted", (signal) => {
+			setCallAccepted(true)
+			peer.signal(signal)
+		})
+
+		connectionRef.current = peer
+	}
+
+	const answerCall =() =>  {
+		setCallAccepted(true)
+		const peer = new Peer({
+			initiator: false,
+			trickle: false,
+			stream: stream
+		})
+		peer.on("signal", (data) => {
+			socket.emit("answerCall", { signal: data, to: caller })
+		})
+		peer.on("stream", (stream) => {
+			if (userVideo.current) {
+				setTimeout(() => {
+					userVideo.current.srcObject = stream;
+				}, 100);
+			} else {
+				console.error("userVideo reference is not set yet.");
+			}
+		})
+
+		peer.signal(callerSignal)
+		connectionRef.current = peer
+	}
+
+	const leaveCall = () => {
+		setCallEnded(true)
+		connectionRef.current.destroy()
+	}
+
+	return (
+		<>
+			<h1 style={{ textAlign: "center", color: '#fff' }}>Zoomish</h1>
+		<div className="container">
+			<div className="video-container">
+				<div className="video">
+					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+				</div>
+				<div className="video">
+					{callAccepted && !callEnded ?
+					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
+					null}
+				</div>
+			</div>
+			<div className="myId">
+				
+				<input
+					id="filled-basic"
+					placeholder="Name"
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					style={{ marginBottom: "20px" }}
+				/>
+				<span>{me}</span>
+
+				<input
+					id="filled-basic"
+					placeholder="ID to call"
+					value={idToCall}
+					onChange={(e) => setIdToCall(e.target.value)}
+				/>
+				<div className="call-button">
+					{callAccepted && !callEnded ? (
+						<button  color="secondary" onClick={leaveCall}>
+							End Call
+						</button>
+					) : (
+						<button aria-label="call" onClick={() => callUser(idToCall)}>
+							Phone
+						</button>
+					)}
+					{idToCall}
+				</div>
+			</div>
+			<div>
+				{receivingCall && !callAccepted ? (
+						<div className="caller">
+						<h1 >{name} is calling...</h1>
+						<button onClick={answerCall}>
+							Answer
+						</button>
+					</div>
+				) : null}
+			</div>
+		</div>
+		</>
+	)
 }
 
-export default App;
+export default App
